6e2448e0892face3555aa4b46082d5561ca3d506
unknown <xavierquispezamorano@gmail.com>
2022-04-25 17:35:57 -0500

    first commit

--- src/main/java/edu/ilp/sysgailp/entity/Estudiante.java
+++ src/main/java/edu/ilp/sysgailp/entity/Estudiante.java
@@ -1,2 +1,53 @@
-package edu.ilp.sysgailp.entity;public class Estudiante {
+package edu.ilp.sysgailp.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "estudiante")
+@PrimaryKeyJoinColumn(referencedColumnName = "IDPERSONA")
+public class Estudiante extends Persona {
+    @Column(name = "codigo",length = 18, nullable = false)
+    private String codigo;
+    @Column(name = "serie",length = 18)
+    private String serie;
+
+    public Estudiante() {
+    }
+
+    public Estudiante(String codigo, String serie) {
+        this.codigo = codigo;
+        this.serie = serie;
+    }
+
+    public Estudiante(Long idpersona, String codigo, String serie) {
+        super(idpersona);
+        this.codigo = codigo;
+        this.serie = serie;
+    }
+
+    public Estudiante(String nombre, String apellido, int edad, String dni, Date fechaNacimiento, String genero, String codigo, String serie) {
+        super(nombre, apellido, edad, dni, fechaNacimiento, genero);
+        this.codigo = codigo;
+        this.serie = serie;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getSerie() {
+        return serie;
+    }
+
+    public void setSerie(String serie) {
+        this.serie = serie;
+    }
 }
